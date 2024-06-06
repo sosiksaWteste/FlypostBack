72c1b5af1edd39bf4020b00b29c48d13f233985a
sosiksaWteste <111946557+sosiksaWteste@users.noreply.github.com>
2024-06-07 00:26:18 +0300

    Update insertScript.sql
    fixed according to new db

--- flypost_backend/insertScript.sql
+++ flypost_backend/insertScript.sql
@@ -1,13 +1,14 @@
 use flypost;
 
-insert into user(login, password) values('admin', 'admin');
-insert into user(login, password) values('user', 'user');
-insert into user(login, password) values('user2', 'user2');
-insert into user(login, password) values('work', 'work');
+insert into user(login, password, role) values('admin', 'admin', 2);
+insert into user(login, password, role) values('user', 'user', 0);
+insert into user(login, password, role) values('user2', 'user2', 0);
+insert into user(login, password, role) values('work', 'work', 1);
 
 insert into city(name) values ('Kyiv');
 
 insert into office(office_number, address, cord_x, cord_y, city_id) values (23, '63 apt; mac st.', 50.033904, 36.208018, 1);
+insert into office(office_number, address, cord_x, cord_y, city_id) values (24, '65 apt; mcconell st.', 50.033904, 36.208018, 1);
 
 insert into client(first_name, last_name, phone, email, user_id) values('alex', 'kister', '099234585', 'aaa@gmail.com', 2);
 insert into client(first_name, last_name, phone, email, user_id) values('maxim', 'kister', '099234335', 'mmm@gmail.com', 3);
@@ -18,4 +19,4 @@ insert into package(width, length, height, weight, insurance) values(100, 100, 5
 
 insert into payment(payment_data) values('mastercard');
 
-insert into delivery(package_id, sender_id, recipient_id, payment_id, send_date, send_from, send_to, price, current_position) values(1, 1, 2, 1, '2023-01-01', 'Kyiv', 'Kharkiv', 2500, 'виїхала');
+insert into delivery(package_id, sender_id, recipient_id, payment_id, send_date, send_from, send_to, price, current_position) values(1, 1, 2, 1, '2023-01-01', 1, 2, 2500, 'виїхала');
