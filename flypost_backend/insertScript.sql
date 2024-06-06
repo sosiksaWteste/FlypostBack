dda8519324e75a75454ecc33d155af6d0c4e40b4
sosiksaWteste <111946557+sosiksaWteste@users.noreply.github.com>
2024-06-06 19:53:53 +0300

    Update insertScript.sql
    db fix

--- flypost_backend/insertScript.sql
+++ flypost_backend/insertScript.sql
@@ -7,7 +7,7 @@ insert into user(login, password) values('work', 'work');
 
 insert into city(name) values ('Kyiv');
 
-insert into office(office_number, address, city_id) values (23, '63 apt; mac st.', 1);
+insert into office(office_number, address, cord_x, cord_y, city_id) values (23, '63 apt; mac st.', 50.033904, 36.208018, 1);
 
 insert into client(first_name, last_name, phone, email, user_id) values('alex', 'kister', '099234585', 'aaa@gmail.com', 2);
 insert into client(first_name, last_name, phone, email, user_id) values('maxim', 'kister', '099234335', 'mmm@gmail.com', 3);
@@ -18,4 +18,4 @@ insert into package(width, length, height, weight, insurance) values(100, 100, 5
 
 insert into payment(payment_data) values('mastercard');
 
-insert into delivery(package_id, sender_id, recipient_id, payment_id, send_date, send_from, send_to, price) values(1, 1, 2, 1, '2023-01-01', 'Kyiv', 'Kharkiv', 2500);
+insert into delivery(package_id, sender_id, recipient_id, payment_id, send_date, send_from, send_to, price, current_position) values(1, 1, 2, 1, '2023-01-01', 'Kyiv', 'Kharkiv', 2500, 'виїхала');
