32a770f064a53a3b6f6cf21b4d3974e51a8fd1f8
sosiksaWteste <111946557+sosiksaWteste@users.noreply.github.com>
2024-06-06 19:19:29 +0300

    Initial commit
    (db creation, insertion; server v1)

---
+++ flypost_backend/index.js
@@ -0,0 +1,103 @@
+const express = require('express');
+const mysql = require('mysql');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+const port = 3001;
+
+app.use(cors());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'administrator',
+    database: 'flypost'
+});
+
+db.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log('Connected to database');
+});
+
+app.get('/users', (req, res) => {
+    db.query('SELECT * FROM user', (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        res.json(rows);
+    });
+});
+
+app.get('/clients', (req, res) => {
+    db.query('SELECT * FROM client', (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        res.json(rows);
+    });
+});
+
+app.get('/employees', (req, res) => {
+    db.query('SELECT * FROM employee', (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        res.json(rows);
+    });
+});
+
+app.get('/packages', (req, res) => {
+    db.query('SELECT * FROM package', (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        res.json(rows);
+    });
+});
+
+app.get('/deliveries', (req, res) => {
+    db.query('SELECT * FROM delivery', (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        res.json(rows);
+    });
+});
+
+app.get('/payments', (req, res) => {
+    db.query('SELECT * FROM payment', (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        res.json(rows);
+    });
+});
+
+app.post('/payments', (req, res) => {
+    let data = req.body.toString();
+
+    db.query(`INSERT INTO payment(payment_data) values(${data});`, (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        res.status(200);
+    });
+});
+
+app.post('/users', (req, res) => {
+    let data = req.body.toString();
+
+    db.query(`INSERT INTO payment(payment_data) values(${data});`, (err, rows) => {
+        if (err) {
+            throw err;
+        }
+        res.status(200);
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
